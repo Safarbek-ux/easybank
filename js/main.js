d646f578b54b4efbd3374cc7b57e16c102495b6f
safarbek-ux <safarbekyunusov@gmail.com>
2023-11-18 16:38:26 +0500

    Modal menu qo'shildi

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 let hamburger = document.querySelector('.hamburger');
 let open = document.querySelector('.open');
 let close = document.querySelector('.close');
+let modal = document.querySelector('.modal');
+let mobile_List = document.querySelector('.mobile_list');
 hamburger.addEventListener('click', () => {
     open.classList.toggle('hidden');
-    close.classList.toggle('block')
+    close.classList.toggle('block');
+    modal.classList.toggle('flex');
+    mobile_List.classList.toggle('animatemenu')
 })
